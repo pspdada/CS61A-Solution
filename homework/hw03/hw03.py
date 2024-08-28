9da5e98ca6efbcde164edfb7e97cc93a8165fd3f
psp_dada <1824427006@qq.com>
2024-08-28 16:07:20 +0800

    正在做 cats

--- homework/hw03/hw03.py
+++ homework/hw03/hw03.py
@@ -1,4 +1,4 @@
-HW_SOURCE_FILE=__file__
+HW_SOURCE_FILE = __file__
 
 
 def pascal(row, column):
@@ -14,6 +14,12 @@ def pascal(row, column):
     6
     """
     "*** YOUR CODE HERE ***"
+    if column == 0 or row == column:
+        return 1
+    elif column > row:
+        return 0
+    else:
+        return pascal(row - 1, column - 1) + pascal(row - 1, column)
 
 
 def insert_items(s, before, after):
@@ -42,33 +48,43 @@ def insert_items(s, before, after):
     True
     """
     "*** YOUR CODE HERE ***"
+    index = 0
+    while index < len(s):
+        if s[index] == before:
+            s.insert(index + 1, after)
+            index += 1
+        index += 1
+    return s
 
 
-HW_SOURCE_FILE=__file__
+HW_SOURCE_FILE = __file__
 
 
 def planet(mass):
     """Construct a planet of some mass."""
     assert mass > 0
     "*** YOUR CODE HERE ***"
+    return ["planet", mass]
+
 
 def mass(p):
     """Select the mass of a planet."""
-    assert is_planet(p), 'must call mass on a planet'
+    assert is_planet(p), "must call mass on a planet"
     "*** YOUR CODE HERE ***"
+    return p[1]
+
 
 def is_planet(p):
     """Whether p is a planet."""
-    return type(p) == list and len(p) == 2 and p[0] == 'planet'
+    return type(p) == list and len(p) == 2 and p[0] == "planet"
+
 
 def examples():
-    t = mobile(arm(1, planet(2)),
-               arm(2, planet(1)))
-    u = mobile(arm(5, planet(1)),
-               arm(1, mobile(arm(2, planet(3)),
-                             arm(3, planet(2)))))
+    t = mobile(arm(1, planet(2)), arm(2, planet(1)))
+    u = mobile(arm(5, planet(1)), arm(1, mobile(arm(2, planet(3)), arm(3, planet(2)))))
     v = mobile(arm(4, t), arm(2, u))
     return t, u, v
+
 
 def total_mass(m):
     """Return the total mass of m, a planet or mobile.
@@ -87,7 +103,8 @@ def total_mass(m):
         assert is_mobile(m), "must get total mass of a mobile or a planet"
         return total_mass(end(left(m))) + total_mass(end(right(m)))
 
-def balanced(m):
+
+def balanced(m) -> bool:
     """Return whether m is balanced.
 
     >>> t, u, v = examples()
@@ -108,10 +125,17 @@ def balanced(m):
     True
     """
     "*** YOUR CODE HERE ***"
+    l, r = left(m), right(m)
+    if is_planet(m):
+        return True
+    elif length(l) * total_mass(l) != length(r) * total_mass(r):
+        return False
+    else:
+        return True and balanced(end(l)) and balanced(end(r))
 
 
 def berry_finder(t):
-    """Returns True if t contains a node with the value 'berry' and 
+    """Returns True if t contains a node with the value 'berry' and
     False otherwise.
 
     >>> scrat = tree('berry')
@@ -128,9 +152,17 @@ def berry_finder(t):
     True
     """
     "*** YOUR CODE HERE ***"
+    if label(t) == "berry":
+        return True
+    else:
+        is_exist = [berry_finder(b) for b in branches(t)]
+        if True in is_exist:
+            return True
+        else:
+            return False
 
 
-HW_SOURCE_FILE=__file__
+HW_SOURCE_FILE = __file__
 
 
 def max_path_sum(t):
@@ -144,10 +176,27 @@ def max_path_sum(t):
     """
     "*** YOUR CODE HERE ***"
 
+    def find_path(t, now_value) -> int:
+        now_value += label(t)
+        if is_leaf(t):
+            return now_value
+        else:
+            return max(find_path(b, now_value) for b in branches(t))
+
+    return find_path(t, 0)
+
 
 def print_move(origin, destination):
     """Print instructions to move a disk."""
     print("Move the top disk from rod", origin, "to rod", destination)
+
+
+def find_middle(start, end):
+    numbers = {1, 2, 3}
+    used_numbers = {start, end}
+    middle = list(numbers - used_numbers)[0]
+    return middle
+
 
 def move_stack(n, start, end):
     """Print the moves required to move n disks on the start pole to the end
@@ -177,10 +226,18 @@ def move_stack(n, start, end):
     Move the top disk from rod 1 to rod 3
     """
     assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    middle = find_middle(start, end)
+    if n == 1:
+        print_move(start, end)
+    else:
+        move_stack(n - 1, start, middle)
+        move_stack(1, start, end)
+        move_stack(n - 1, middle, end)
     "*** YOUR CODE HERE ***"
 
 
 from operator import sub, mul
+
 
 def make_anonymous_factorial():
     """Return the value of an expression that computes factorial.
@@ -193,42 +250,49 @@ def make_anonymous_factorial():
     ...     ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'FunctionDef', 'Recursion'])
     True
     """
-    return 'YOUR_EXPRESSION_HERE'
+    return (lambda f: f(f))(lambda f: lambda x: 1 if x == 0 else x * f(f)(x - 1))
 
 
 def mobile(left, right):
     """Construct a mobile from a left arm and a right arm."""
     assert is_arm(left), "left must be an arm"
     assert is_arm(right), "right must be an arm"
-    return ['mobile', left, right]
+    return ["mobile", left, right]
+
 
 def is_mobile(m):
     """Return whether m is a mobile."""
-    return type(m) == list and len(m) == 3 and m[0] == 'mobile'
+    return type(m) == list and len(m) == 3 and m[0] == "mobile"
+
 
 def left(m):
     """Select the left arm of a mobile."""
     assert is_mobile(m), "must call left on a mobile"
     return m[1]
 
+
 def right(m):
     """Select the right arm of a mobile."""
     assert is_mobile(m), "must call right on a mobile"
     return m[2]
 
+
 def arm(length, mobile_or_planet):
     """Construct an arm: a length of rod with a mobile or planet at the end."""
     assert is_mobile(mobile_or_planet) or is_planet(mobile_or_planet)
-    return ['arm', length, mobile_or_planet]
+    return ["arm", length, mobile_or_planet]
+
 
 def is_arm(s):
     """Return whether s is an arm."""
-    return type(s) == list and len(s) == 3 and s[0] == 'arm'
+    return type(s) == list and len(s) == 3 and s[0] == "arm"
+
 
 def length(s):
     """Select the length of an arm."""
     assert is_arm(s), "must call length on an arm"
     return s[1]
+
 
 def end(s):
     """Select the mobile or planet hanging at the end of an arm."""
@@ -236,22 +300,25 @@ def end(s):
     return s[2]
 
 
-
 # Tree Data Abstraction
+
 
 def tree(label, branches=[]):
     """Construct a tree with the given label value and a list of branches."""
     for branch in branches:
-        assert is_tree(branch), 'branches must be trees'
+        assert is_tree(branch), "branches must be trees"
     return [label] + list(branches)
+
 
 def label(tree):
     """Return the label value of a tree."""
     return tree[0]
 
+
 def branches(tree):
     """Return the list of branches of the given tree."""
     return tree[1:]
+
 
 def is_tree(tree):
     """Returns True if the given tree is a tree, and False otherwise."""
@@ -262,11 +329,13 @@ def is_tree(tree):
             return False
     return True
 
+
 def is_leaf(tree):
     """Returns True if the given tree's list of branches is empty, and False
     otherwise.
     """
     return not branches(tree)
+
 
 def print_tree(t, indent=0):
     """Print a representation of this tree in which each node is
@@ -287,9 +356,10 @@ def print_tree(t, indent=0):
       6
         7
     """
-    print('  ' * indent + str(label(t)))
+    print("  " * indent + str(label(t)))
     for b in branches(t):
         print_tree(b, indent + 1)
+
 
 def copy_tree(t):
     """Returns a copy of t. Only for testing purposes.
@@ -301,4 +371,3 @@ def copy_tree(t):
     5
     """
     return tree(label(t), [copy_tree(b) for b in branches(t)])
-
